57119d585925e28720e181bf17315525c3a8bb73
rafi1105 <111092930+rafi1105@users.noreply.github.com>
2022-08-25 23:58:07 +0600

    array

---
+++ array_2D/Decimal_to_bainary.c
@@ -0,0 +1,16 @@
+// C Program to Convert Decimal to Binary Using Arrays
+#include <stdio.h>
+int main(){
+    int arr[10], num, i, j;
+    printf("Enter a decimal number: ");
+    scanf("%d", &num);
+    for (i = 0; num > 0; i++){
+        arr[i] = num % 2;
+        num = num / 2;
+    }
+    printf("binary number is: ");
+    for (j = i - 1; j >= 0; j--){
+        printf("%d", arr[j]);
+    }
+    return 0;
+}
