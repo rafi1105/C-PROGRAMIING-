a74be016adbf92d0f2b9a7b7ebd4e150c8c6af14
Rafi_kabir <111092930+rafi1105@users.noreply.github.com>
2022-09-04 16:43:04 +0600

    rk

---
+++ Project/Super shop billing system.c/test-01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+int a;
+float x;
+ printf("\20*******************  WELCOME TO RK SUPER SHOP **************************\20\n");
+printf("\t\t    PLEASE CHOICE YOUR PRODUCT :\20\n");
+printf("\t\t  \20-------------------------------\20\n");
+
+}
