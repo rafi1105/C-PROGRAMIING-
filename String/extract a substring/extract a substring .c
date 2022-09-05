c9fa93fe8698edaae23a0be51f04ea600190027a
Rafi_kabir <111092930+rafi1105@users.noreply.github.com>
2022-09-05 23:30:00 +0600

    RK

--- String/extract a substring/extract a substring .c
+++ String/extract a substring/extract a substring .c
@@ -1,57 +1,28 @@
 #include <stdio.h>
-
-/*Function declaration*/
-int getSubString(char *source, char *target,int from, int to);
-
-int main()
+void main() 
 {
-	char text [100]={0};
-	char text1[50] ={0};
-	int from,to;
-	
-	printf("Enter a string: ");
-	fgets(text,100,stdin);
-	
-	printf("Enter from index: ");
-	scanf("%d",&from);
-	printf("Enter to index: ");
-	scanf("%d",&to);	
-	
-	printf("String is: %s\n",text);
-	
-	if(getSubString(text,text1,from,to)==0)
-	    printf("Substring is: %s\n",text1);
-    else
-        printf("Function execution failed!!!\n");
-	
-	return 0;
-}
-
-/*Function definition*/
-int  getSubString(char *source, char *target,int from, int to)
-{
-	int length=0;
-	int i=0,j=0;
-	
-	//get length
-	while(source[i++]!='\0')
-		length++;
-	
-	if(from<0 || from>length){
-		printf("Invalid \'from\' index\n");
-		return 1;
-	}
-	if(to>length){
-		printf("Invalid \'to\' index\n");
-		return 1;
-	}	
-	
-	for(i=from,j=0;i<=to;i++,j++){
-		target[j]=source[i];
-	}
-	
-	//assign NULL at the end of string
-	target[j]='\0'; 
-	
-	return 0;	
+   char str[100], sstr[100];
+   int pos, l, c = 0;
+   
+       printf("\n\nExtract a substring from a given string:\n");
+       printf("--------------------------------------------\n");  
+ 
+       printf("Input the string : ");
+       scanf ("%[^\n]", str);
+ 
+   printf("Input the position to start extraction :");
+   scanf("%d", &pos);
+   
+   printf("Input the length of substring :");
+   scanf("%d", &l);
+ 
+   while (c < l) 
+   {
+      sstr[c] = str[pos+c-1];
+      c++;
+   }
+   sstr[c] = '\0';
+ 
+   printf("The substring retrieve from the string is :  %s \n\n", sstr);
+ 
 }
