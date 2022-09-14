3242eeb08fc77441d2500f7523b0dfff12dbba37
rafi1105 <111092930+rafi1105@users.noreply.github.com>
2022-09-14 20:37:11 +0600

    rk

--- String/Palindrome/palindrome.c
+++ String/Palindrome/palindrome.c
@@ -2,6 +2,23 @@
 int main ()
 {
     char ch1 [20];
-    int length=0,flag=1;
-    
+    int length=0,flag=1,i;
+    scanf("%[^\n]",ch1);
+    for (i= 0;ch[i]!='\0';i++)
+    {
+        length++;
+    }
+    for(i=0;i<length/2;i++)
+    {
+        if(ch1[i]!=ch1[length-1-i])
+        {
+            flag=0;
+        }
+    }
+    if (flag==1)
+    {
+        printf("palindrome");
+    }
+    else 
+    printf("not palindrome");
 }
