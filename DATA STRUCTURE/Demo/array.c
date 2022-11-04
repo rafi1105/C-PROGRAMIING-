8bf616b64741e57ef5484a8f212f405bbf8c81e4
Rafi_kabir <111092930+rafi1105@users.noreply.github.com>
2022-11-04 22:13:37 +0600

    rk

---
+++ DATA STRUCTURE/Demo/array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int printArray ( int array[], int size )
+{
+    printf (" the array is.... \n");
+    for ( int i=0; i<size; i++ )
+    {
+        printf ("%d ", array[i]);
+    }
+}
+int InputArray ( int array [], int size)
+{
+    printf (" please input the array .......\n");
+    for ( int i=0; i<size; i++ )
+    {
+        scanf ("%d", &array[i]);
+    }
+}
+
+int main ()
+{
+    int array[30],size;
+    printf (" please input the array size .......\n");
+    scanf ("%d", &size);
+   InputArray ( array , size);
+   printArray ( array,size);
+    return 0;
+
+}
