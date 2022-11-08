8d8d43d3233efa02292491b132b330e9f1bb56f0
rafi1105 <111092930+rafi1105@users.noreply.github.com>
2022-11-08 23:30:26 +0600

    rk

--- DATA STRUCTURE/Linked List/run.c
+++ DATA STRUCTURE/Linked List/run.c
@@ -12,16 +12,18 @@ int main ()
     int n;
     scanf ("%d",&n);
     head = NULL;
-     for ( int i=0; i < n; i++ )
+     
     fnode =(struct Node*) malloc (sizeof(struct node));
     printf ( " enter the number of data nodes\n" );
     scanf("%d", & fnode -> data );
     fnode -> next = NULL;
     if ( head == NULL )
     head=temp= fnode;
+  
     else 
-      
+        for ( int i=1; i < n; i++ )
         {
+            
         temp -> next = fnode;
         temp= fnode;
         }
