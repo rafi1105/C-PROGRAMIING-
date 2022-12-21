f42d5ade090f6894fcc4b102da4df79f45c82cdf
Rafi_kabir <111092930+rafi1105@users.noreply.github.com>
2022-12-21 13:22:14 +0600

    Update project stack.c

--- DATA STRUCTURE/Stack/project stack.c
+++ DATA STRUCTURE/Stack/project stack.c
@@ -1,23 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct Node { 
+struct Node
+{
     int data;
     struct Node *next;
 }*head=NULL;
+
 void option();
-int empty ()
+void push(int value);
+void pop();
+void peek();
+void display();
+int main()
 {
-    if (head==NULL)
+    option();
+}
+void option()
+{
+     int ch,value;
+     while (1)
     {
-        printf("\n the stack is empty\n");
-        return 1;
-    }
-    else if (head !=NULL)
-    {
-        return 0;
+     printf( "\n 1. Push\n 2. Pop\n 3. peek\n 4. Display\n 5. Exit\n");
+    printf ("Enter your choice: ");
+    scanf("%d", &ch);
+    printf ("\n");
+    
+        switch (ch)
+        {
+        case 1:
+        {
+            printf ("Enter your value -> ");
+            scanf("%d", &value);
+            push(value);
+        }
+            break;
+       case 2:
+        {
+           // peek();
+            pop();
+        }
+        break;
+        
+        case 3:
+            peek();
+        break;
+        
+        case 4: 
+            display();
+        break;
+        
+        case 5: exit(0);
+        break;
+        default:
+         printf ("Invalid option enter 1 -> 5 ");
+            break;
+        }
     }
 }
-int push (int value)
+void push (int value)
 {
     struct Node *newnode =(struct Node*) malloc (sizeof(struct Node));
     struct Node *temp;
@@ -31,12 +71,61 @@ int push (int value)
     }
     else 
     {
-       newnode->next = head;
-       head=newnode;
+        while (temp->next!=NULL)
+        {
+            temp=temp->next;
+        }
+        
+       temp->next = newnode;
+       
         printf(" push node-> %d\n",newnode->data);
     }
      option ();
     }
+void pop()
+{
+    struct Node* temp,*ptr;
+    temp = head;
+    if (head==NULL)
+    {
+        printf ("stack is empty");
+    }
+    if (temp->next==NULL) {
+        head=NULL;
+        printf("Now stack is empty");
+    }
+    else 
+    {
+        while (temp ->next != NULL)
+        {
+            ptr=temp;
+            temp=temp-> next;
+        }
+        printf("Pop Number is -> %d", temp->data);
+        ptr->next=NULL;
+    }
+     option ();
+}
+void display()
+{
+    struct Node *temp;
+    temp=head;
+    if (head==NULL)
+    {
+        printf ("\n stack is empty\n");
+
+    }
+
+    else {
+        while (temp->next !=NULL)
+        {
+            printf("list is -> %d\n", temp->data);
+            temp=temp-> next;
+        }
+        printf("list is -> %d\n", temp->data);
+        
+    }
+}
 void peek ()
 {
     struct Node*temp=head;
@@ -54,86 +143,4 @@ void peek ()
     }
 
      option ();
-}
-void pop ()
-{
-    struct Node* temp,*ptr;
-    temp = head;
-    if (!empty())
-    {
-        empty();
-    }
-    else 
-    {
-        while (temp ->next != NULL)
-        {
-            ptr=temp;
-            temp=temp-> next;
-        }
-        printf("Pop Number is -> %d", temp->data);
-        ptr->next=NULL;
-    }
-     option ();
-}
-void show()
-{
-   struct Node* temp;
-   temp = head;
-   if (head == NULL)
-   {
-    printf(" empty stack\n");
-   }
-   else if(temp->next= NULL)
- printf("\n list of the stack is ->%d \n", temp->data);
-   else 
-   {
-         while (temp->next  != NULL)
-   {
-    printf("\n list of the stack is ->%d \n", temp->data);
-    temp = temp->next;
-   }
-   }
-  
-    option ();
-}
-void option()
-{
-     int ch,value;
-    printf ("Enter your choice: ");
-    scanf("%d", &ch);
-    printf ("\n");
-    while (1)
-    {
-        switch (ch)
-        {
-        case 1:
-        {
-            printf ("Enter your value -> ");
-            scanf("%d", &value);
-            push(value);
-        }
-            break;
-        case 2:
-        {
-           // peek();
-            pop();
-        }
-        break;
-        case 3:
-            peek();
-        break;
-        case 4: 
-            show();
-        break;
-        case 5: exit(0);
-        break;
-        default:
-         printf ("Invalid option enter 1 -> 5 ");
-            break;
-        }
-    }
-}
-int main ()
-{
-    option ();
 }
